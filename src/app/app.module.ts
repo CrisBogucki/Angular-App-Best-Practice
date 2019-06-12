f10fd4a9510b12740aa4250103bbd3073e5480fd
Krzysztof Bogucki <krzysztof.lubien@icloud.com>
2019-06-12 17:26:01 +0200

    add bulma css framework free js

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,36 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {HTTP_INTERCEPTORS} from '@angular/common/http';
+import {JwtInterceptor} from './@core/interceptors/jwt.interceptor';
+import {ErrorInterceptor} from './@core/interceptors/error.interceptor';
+
+import {FooterComponent} from './@core/ui/footer/footer.component';
+import {NavComponent} from './@core/ui/nav/nav.component';
+import {BreadcrumbComponent} from './@core/ui/breadcrumb/breadcrumb.component';
+import {MenuComponent} from './@core/ui/menu/menu.component';
+
+import {AppComponent} from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        FooterComponent,
+        NavComponent,
+        BreadcrumbComponent,
+        MenuComponent
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule
+    ],
+    providers: [
+        {provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true},
+        {provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true},
+    ],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
