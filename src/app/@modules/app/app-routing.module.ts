aecde5800cf0a31798ff99bd5c16b13e6800ee73
Krzysztof Bogucki <krzysztof.lubien@icloud.com>
2019-07-16 13:10:00 +0200

    add add child outlet router in layout

--- src/app/@modules/app/app-routing.module.ts
+++ src/app/@modules/app/app-routing.module.ts
@@ -1,13 +1,20 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
 import {LayoutComponent} from './layout/layout.component';
+import {DashboardComponent} from './dashboard/dashboard.component';
 
 const routes: Routes = [
-  { path: '', component: LayoutComponent}
+    {
+        path: '', component: LayoutComponent, children: [
+            {path: 'dashboard', component: DashboardComponent},
+            { path: '', redirectTo: 'dashboard'}
+        ]
+    }
 ];
 
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forChild(routes)],
+    exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
